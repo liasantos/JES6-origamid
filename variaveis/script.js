95ba48decb9eab0fa413f1686dc69b876a1b7cba
Lia Silva <lia_silva@sicredi.com.br>
2022-02-08 12:34:52 -0300

    feat: add variables

---
+++ variaveis/script.js
@@ -0,0 +1,11 @@
+var nome = 'Lia';
+var idade = 27;
+var cursaFaculdade = true;
+
+console.log(nome, idade, cursaFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(totalPreco);
