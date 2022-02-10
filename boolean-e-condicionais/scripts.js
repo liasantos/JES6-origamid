f5ba233146f97bd2514fc37cba5eca1f9039a618
Lia Silva <lia_silva@sicredi.com.br>
2022-02-10 15:02:42 -0300

    truthy e falsy

--- boolean-e-condicionais/scripts.js
+++ boolean-e-condicionais/scripts.js
@@ -2,9 +2,41 @@
 //condicional if else
 
 var possuiGraduacao = true;
+var possuiDoutorado = false;
 
-if(possuiGraduacao) {
+if(possuiDoutorado){
+    console.log('Possui graduação e doutorado');
+} else if(possuiGraduacao) {
     console.log('Possui graduação');
 } else {
-    console.log('Nçao possui graduação');
-} //retorna Possui Graduação e não executa o else
+    console.log('Não possui graduação');
+} 
+
+//Truthy e Falsy
+//Falsy
+//if(false)
+//if(0) // ou -0
+//if(NaN)
+//if(null)
+//if(undefined)
+//if('') // ou "" ou `` - sem espaço entre
+//todo o resto é truthy
+//exemplo
+var nome = '';
+if(nome) {
+    console.log(nome);
+} else {
+    console.log('Nome não existe');
+} //retorna Nome não existe
+
+//Truthy
+//if(true)
+//if(1)
+//if(' ') // com espaço entre
+//if('lia')
+//if(-5)
+//if({})
+
+//operador de negação
+// ! na frente da expressão
+//para verificar se é truthy ou falsy !! na frente
