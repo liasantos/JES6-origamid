685f27201cea931bf788a5019f048d5f72f5ee13
Lia Silva <lia_silva@sicredi.com.br>
2022-02-10 14:21:33 -0300

    aula boolean / condicionais

---
+++ boolean-e-condicionais/scripts.js
@@ -0,0 +1,10 @@
+//boolean - true ou false
+//condicional if else
+
+var possuiGraduacao = true;
+
+if(possuiGraduacao) {
+    console.log('Possui graduação');
+} else {
+    console.log('Nçao possui graduação');
+} //retorna Possui Graduação e não executa o else
