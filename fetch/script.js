55e24e759708ba764975554ce568f75877678d96
liasantos <liasilva77@acad.charqueadas.ifsul.edu.br>
2022-11-30 23:30:31 -0300

    feat: aula 2 de fetch

--- fetch/script.js
+++ fetch/script.js
@@ -71,3 +71,72 @@ const sobre = fetch("./sobre.html")
     const titulo = div.querySelector("h1");
     document.querySelector("h1").innerText = titulo.innerText;
   });
+
+  // .Blob()
+// Um blob é um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser
+//utilizado para transformarmos requisições de imagens por exemplo. O blob gera um URL único.
+
+const divBlob = document.createElement("div");
+
+const imagem = fetch("./imagem.png");
+  imagem.then((response) => response.blob())
+  .then((imgBlob) => {
+    const blobUrl = URL.createObjectURL(imgBlob);
+    const imagemDom = document.querySelector('img');
+    imagemDom.src = blobUrl;
+    console.log(blobUrl);
+  });
+
+// .Clone()
+// Ao utilizarmos os métodos acima, text, json e blob, a resposta é modificada. Por isso existe o
+//método clone, caso você necessite transformar uma resposta em diferentes valores.
+
+const divClone = document.createElement("div");
+
+const cepClone = fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
+  const cloneResponse = response.clone();
+  response.json().then((json) => {
+    console.log(json);
+  });
+  cloneResponse.text().then((text) => {
+    console.log(text);
+  });
+});
+
+// .Headers
+// É uma propriedade que possui os cabeçalhos da requisição. É um tipo de dado iterável então podemos
+// utilizar o forEach para vermos cada um deles.
+
+const divHeaders = document.createElement("div");
+
+const cepHeaders = fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
+  response.headers.forEach(console.log);
+});
+
+// . Status e .Ok
+// Retorna o status da requisição. Se foi 404, 200, 202 e mais. ok retorna um valor booleano sendo
+// true para uma requisição de sucesso e false para uma sem sucesso.
+
+const divStatus = document.createElement("div");
+
+fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
+  console.log(response.status, response.ok, response);
+  if (response.status === 404) {
+    console.log("Página não encontrada");
+  }
+});
+
+// . Url e .Type
+// .url retorna o url da requisição. .type retorna o tipo da reposta.
+
+const divUrl = document.createElement("div");
+
+fetch("https://viacep.com.br/ws/01001000/json/").then((response) => {
+  console.log(response.type, response.url);
+});
+
+//types
+// basic: feito na mesma origem
+// cors: feito em url body pode estar disponível
+// error: erro de conexão
+// opaque: no-cors, não permite acesso de outros sites
